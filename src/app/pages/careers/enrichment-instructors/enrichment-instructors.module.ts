0fe151211d7cf36d3b2acddabdab798ae343515a
Charlotte Sargent <17654@somerville.qld.edu.au>
2020-09-29 10:16:56 +1000

    updated careers page and enrichment instructor

--- src/app/pages/careers/enrichment-instructors/enrichment-instructors.module.ts
+++ src/app/pages/careers/enrichment-instructors/enrichment-instructors.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 import { ComponentsModule } from 'src/app/components/components.module';
@@ -14,6 +14,7 @@ import { EnrichmentInstructorsPage } from './enrichment-instructors.page';
     FormsModule,
     IonicModule,
     ComponentsModule,
+    ReactiveFormsModule,
     EnrichmentInstructorsPageRoutingModule
   ],
   declarations: [EnrichmentInstructorsPage]
