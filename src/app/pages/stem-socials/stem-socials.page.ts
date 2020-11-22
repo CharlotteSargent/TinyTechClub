3b1bb132f5470413638465be111f0e0ce84df5ee
Charlotte Sargent <charlotte_sargent@hotmail.com>
2020-11-22 12:12:13 +1000

    process of making stem socials page

--- src/app/pages/stem-socials/stem-socials.page.ts
+++ src/app/pages/stem-socials/stem-socials.page.ts
@@ -6,8 +6,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stem-socials.page.scss'],
 })
 export class StemSocialsPage implements OnInit {
+  events: any[] = [
+    {
+      eventTitle: "First Event",
+      eventTime: "6:25 PST",
+      eventLocation: "Zoom",
+      eventDescription: `
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus velit adipiscing 
+      duis suspendisse sit purus. Faucibus sodales libero integer eget mauris nibh 
+      odio ullamcorper. Ac ultricies diam urna, aliquet integer. Odio feugiat quis. 
+      In ac elit posuere diam, purus bibendum sed. Mattis vitae feugiat sed sit 
+      cursus urna. Rhoncus vestibulum vel urna, gravida. Blandit nisl volutpat.`,
+      eventMonth: "July",
+      eventDate: 24,
+      eventDay: "Friday",
+    },
+    {
+      eventTitle: "Second Event",
+      eventTime: "7:25 PST",
+      eventLocation: "Zoom",
+      eventDescription: `
+      Lorem ipsum dolor sit , consectetur adipiscing elit. Purus velit adipiscing 
+      duis suspendisse sit purus. Faucibus sodales libero integer  mauris nibh 
+      odio ullamcorper. Ac ultricies diam urna, aliquet integer. Odio  quis. 
+      In ac elit posuere , purus bibendum . Mattis vitae feugiat sed sit 
+      cursus urna. Rhoncus vestibulum vel urna, gravida. Blandit nisl volutpat.`,
+      eventMonth: "July",
+      eventDate: 24,
+      eventDay: "Friday",
+    },
+  ]
+
+  pastEvents: any[] = [
+    {
+      eventTitle: "First Past Event",
+      eventTime: "6:25 PST",
+      eventLocation: "Zoom",
+      eventDescription: `
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus velit adipiscing 
+      duis suspendisse sit purus. Faucibus sodales libero integer eget mauris nibh 
+      odio ullamcorper. Ac ultricies diam urna, aliquet integer. Odio feugiat quis. 
+      In ac elit posuere diam, purus bibendum sed. Mattis vitae feugiat sed sit 
+      cursus urna. Rhoncus vestibulum vel urna, gravida. Blandit nisl volutpat.`,
+      eventMonth: "July",
+      eventDate: 24,
+      eventDay: "Friday",
+    },
+    {
+      eventTitle: "Second Past Event",
+      eventTime: "7:25 PST",
+      eventLocation: "Zoom",
+      eventDescription: `
+      Lorem ipsum dolor sit , consectetur adipiscing elit. Purus velit adipiscing 
+      duis suspendisse sit purus. Faucibus sodales libero integer  mauris nibh 
+      odio ullamcorper. Ac ultricies diam urna, aliquet integer. Odio  quis. 
+      In ac elit posuere , purus bibendum . Mattis vitae feugiat sed sit 
+      cursus urna. Rhoncus vestibulum vel urna, gravida. Blandit nisl volutpat.`,
+      eventMonth: "July",
+      eventDate: 24,
+      eventDay: "Friday",
+    },
+  ];
+  
+  // segmentChanged(ev: any) {
+  //   console.log('Segment changed', ev);
+  // }
 
   constructor() { }
+
 
   ngOnInit() {
   }
