9891cbf91a94de9622aa5c0960ca78aab8a27626
Charlotte Sargent <charlotte_sargent@hotmail.com>
2020-12-20 07:31:51 +1000

    Responsivness of the picture

--- src/app/pages/stem-socials/stem-socials.page.ts
+++ src/app/pages/stem-socials/stem-socials.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+
+import { CommunityPartner } from 'src/app/models/communityPartner.model';
 
 @Component({
   selector: 'app-stem-socials',
@@ -80,10 +83,61 @@ export class StemSocialsPage implements OnInit {
     }
   }
 
-  constructor() { }
-
-
+  communityPartnerForm: FormGroup;
+  formSubmitAttempt: boolean;
+  validationMessages = {
+    firstName: [
+      { type: 'required', message: 'First Name is required' }
+    ],
+    lastName: [
+      { type: 'required', message: 'Last Name is required' }
+    ],
+    email: [
+      { type: 'required', message: 'Email is required' },
+      { type: 'pattern', message: 'Enter a valid email' }
+    ],
+    message: [
+      { type: 'required', message: 'Message is required' }
+    ]
+  };
+​
+  constructor(private formBuilder: FormBuilder) { }
+​
   ngOnInit() {
+    this.communityPartnerForm = this.formBuilder.group({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      phone: new FormControl('', null),
+      linkdin: new FormControl('', null),
+      textbox: new FormControl('', null),
+      workexp: new FormControl('', null),
+      opts: new FormControl('', null),
+      message: new FormControl('', Validators.required)
+    });
   }
+​
+  validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsTouched({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
+    });
+  }
+​
+  submit() {
+    if (this.communityPartnerForm.valid) {
+      console.log('form submitted');
+    } else {
+      this.validateAllFormFields(this.communityPartnerForm);
+    }
+  }
+
 
 }
